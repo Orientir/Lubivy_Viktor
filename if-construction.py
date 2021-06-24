74db82eb10f2fe72397b02544fada956983a9139
Orientir <ya.orient@gmail.com>
2021-06-24 18:43:54 +0300

    string methods

--- if-construction.py
+++ if-construction.py
@@ -1,19 +1,56 @@
-# Типы данных - int, float, str
+# Типы данных - int, float, str, boolean - True, False
 
 # Математические операторы сравнения
 # <, >, <=, >=, !=, ==
 # 4<5, 5<=5, 10>12, 3!=2, 6==6
 
-int_number = 12 # int
-float_number = 12.5 # float
-str_data = "Some string"
+# int_number = 12 # int
+# float_number = 12.5 # float
+# str_data = "Some string"
+#
+# name = input("Input your name from keyboard: ")
+# age = int(input("Input your age from keyboard: "))
+#
+# if age > 18:
+#     print("Ты уже совершеннолетний")
+# elif age > 16:
+#     print("У тебя уже есть паспорт")
+# else:
+#     print("Ты еще ребенок")
 
-name = input("Input your name from keyboard: ")
-age = int(input("Input your age from keyboard: "))
 
-if age > 18:
-    print("Ты уже совершеннолетний")
-elif age > 16:
-    print("У тебя уже есть паспорт")
-else:
-    print("Ты еще ребенок")
+# if - elif - else
+# if number > 30: # ветка
+#     print("Number less 30")
+# elif number > 20: # ветка
+#     print("Number less 20")
+# elif number > 10: # ветка
+#     print("Number less 10")
+#
+#
+# if number > 30:
+#     print("Number more 30")
+#
+#
+# if number > 20:
+#     print("Number more 20")
+#
+#
+# if number > 10:
+#     print("Number more 10")
+
+
+# number = 20
+#
+# if number == 16:
+#     print("Ты получаешь паспорт")
+# elif number == 18:
+#     print("Ты становишся совершеннолетним")
+# elif number == 60:
+#     print("Ты уходишь на пенсию")
+# else:
+#     print("Здесь ниччего не просиходит")
+
+# 2х + 5 = 10
+# a    b    c    -  эти числа вводяться с клавиатуры
+# в ответ пользователю сказать, чему равняется х
---
+++ tasks.py
@@ -0,0 +1,61 @@
+# string = "Hello, "
+# st = "world"
+#
+# new = string + st + "!"# конкатенация
+
+st = "hello world!" # порядковый номер (индекс) - всегда начинается с нуля
+s = "some string"
+# print(st[1])   # []
+# print(st[0:7]) # получить срез, правый крайний индекс не входит
+# print(st[5:]) # получить срез, правый крайний индекс не входит, указываем шаг
+# print(st[::-1]) # обратный индекс
+
+# print(len(st)) # длина строки
+# if_symbol = st.index("z") # ищет подстроку в строке
+# if if_symbol == -1:
+#     print("такого символа в строке нету")
+# else:
+#     print("Индекс элемента: ", if_symbol)
+
+# new = st.replace("world", "friend")
+# print(st)
+# print(new)
+
+# st = st.split(",")
+# print(st)
+
+# if st.istitle():
+#     print("состоит из букв", st.istitle())
+# else:
+#     print("не состоит", st.istitle())
+
+# st = st.upper()
+# print(st)
+
+# st = st.lower()
+# print(st)
+
+# sp = ["h", "e", "l", "l", "o"]
+# print(sp)
+# empty = ""
+# x = "".join(sp)
+# print(x)
+# print(st.startswith("hello"))
+# print(st.endswith("!"))
+
+# symbol = ord("")
+# symbol = chr(1010)
+#
+# print(symbol)
+
+
+
+# # Задача 10
+# # Вводится целое число, обозначающее код символа по таблице ASCII. Определить, это код английской буквы или какой-либо иной символ.
+
+symbol = int(input("Input code "))
+
+if (symbol >= 65 and symbol <= 90) or (symbol >= 97 and symbol <= 122):
+    print(symbol, " - ", chr(symbol), " it is english symbol")
+else:
+    print("it is some symbol: ", chr(symbol))
